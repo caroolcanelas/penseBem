2476a922a017284eb7ea634dd24834b14cddd3c3
carolcanelasg <carolina.canelas@soulasalle.com.br>
2024-06-09 16:16:08 -0300

    lógica do jogo
    funções e constantes que fazem o jogo funcionar

--- my-app/src/Start.js
+++ my-app/src/Start.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+import React from "react";
 var programa, num, tentativa, pontos;
 
 // eslint-disable-next-line no-unused-vars
@@ -17,24 +17,83 @@ function Start() {
     else if (programa == "022") gabarito = gabarito022;
     else {
       alert("Esse programa não existe!");
-      iniciar();
+      return;
     }
 
     pontos = 0;
     num = 1;
     tentativa = 1;
 
-    document.getElementById("visor1").value = `${programa} -> ${num}: `;
-    document.getElementById("visor2").value = `Tentativa ${tentativa} de 3`;
+    atualizarVisor();
+
+    document.getElementById("respostas").style.display = "block";
+  };
+
+  const atualizarVisor = () => {
+    document.getElementById("visor1").innerText = `${programa} -> ${num}: `;
+    document.getElementById("visor2").innerText = `Tentativa ${tentativa} de 3`;
+  };
+
+  const resposta = (resposta) => {
+    const respostaCorreta = gabarito[num - 1];
+    if (resposta === respostaCorreta) {
+      pontos++;
+      num++;
+      tentativa = 1;
+      alert("Resposta Correta");
+      console.log(pontos);
+    } else {
+      tentativa++;
+      alert("Respota errada");
+      if (tentativa > 3) {
+        alert(`Fim do jogo! Você fez ${pontos} pontos.`);
+        // Desabilita os campos de resposta
+        document.getElementById("respostas").style.display = "none";
+      }
+    }
+
+    atualizarVisor();
   };
 
   return (
-    <input
-      type="button"
-      id="botaoStart"
-      value="Start/Reset Jogo"
-      onClick={iniciar}
-    ></input>
+    <div>
+      <p id="visor1">{`${programa} -> ${num}: `}</p>
+      <p id="visor2">{`Tentativa ${tentativa} de 3`}</p>
+      <div id="respostas" style={{ display: "none" }}>
+        <p>
+          <input
+            type="button"
+            value="A"
+            id="botaoA"
+            onClick={() => resposta("A")}
+          ></input>
+          <input
+            type="button"
+            value="B"
+            id="botaoB"
+            onClick={() => resposta("B")}
+          ></input>
+          <input
+            type="button"
+            value="C"
+            id="botaoC"
+            onClick={() => resposta("C")}
+          ></input>
+          <input
+            type="button"
+            value="D"
+            id="botaoD"
+            onClick={() => resposta("D")}
+          ></input>
+        </p>
+      </div>
+      <input
+        type="button"
+        id="botaoStart"
+        value="Start/Reset Jogo"
+        onClick={iniciar}
+      ></input>
+    </div>
   );
 }
 
